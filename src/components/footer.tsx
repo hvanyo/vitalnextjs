3c78746c1f7e0c1a43019f11fc962e5826e62cdf
hvanyo <hvanyo@its.jnj.com>
2022-10-11 09:19:34 -0500

    remove menu/header/footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import {
   on, replaceWith,
 } from '@bodiless/fclasses';
-import { MenuCleanNonEditable } from '@bodiless/vital-navigation-core';
-import {
-  vitalFooter, asFooterToken,
-} from '@bodiless/vital-layout-core';
-import { FooterMenu } from './menu';
+// import { MenuCleanNonEditable } from '@bodiless/vital-navigation-core';
+// import {
+//   vitalFooter, asFooterToken,
+// } from '@bodiless/vital-layout-core';
+// import { FooterMenu } from './menu';
 
 const Copyright = () => (
   <div>
@@ -14,9 +14,9 @@ const Copyright = () => (
   </div>
 );
 
-export const asVanillaFooter = asFooterToken(vitalFooter.Default, {
-  Components: {
-    FooterMenu: on(MenuCleanNonEditable)(FooterMenu),
-    CopyrightRow: replaceWith(Copyright),
-  },
-});
+// export const asVanillaFooter = asFooterToken(vitalFooter.Default, {
+//   Components: {
+//     // FooterMenu: on(MenuCleanNonEditable)(FooterMenu),
+//     CopyrightRow: replaceWith(Copyright),
+//   },
+// });
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,34 +1,40 @@
 import * as React from 'react';
-import { as } from '@bodiless/fclasses';
-import {
-  LayoutClean, vitalLayout, asLayoutToken, vitalFooter
-} from '@bodiless/vital-layout-core';
-import { vitalSpacing } from '@bodiless/vital-elements';
-import { asVanillaHeader } from './header';
-import { asVanillaFooter } from './footer';
+// import { as } from '@bodiless/fclasses';
+// import {
+//   LayoutClean, vitalLayout, asLayoutToken, vitalFooter
+// } from '@bodiless/vital-layout-core';
+// import { vitalSpacing } from '@bodiless/vital-elements';
+// import { asVanillaHeader } from './header';
+// import { asVanillaFooter } from './footer';
 
-const VanillaLayout = asLayoutToken(vitalLayout.Base, {
-  Components: {
-    Header: asVanillaHeader,
-    Footer: asVanillaFooter,
-  },
-  Theme: {
-    ContainerWrapper: as(
-      vitalSpacing.WithSiteMargin,
-      vitalSpacing.WithSiteXLConstraint
-    ),
-    OuterContainer: 'min-h-screen',
-  }
-});
+// const VanillaLayout = asLayoutToken(vitalLayout.Base, {
+//   Components: {
+//     Header: asVanillaHeader,
+//     Footer: asVanillaFooter,
+//   },
+//   Theme: {
+//     ContainerWrapper: as(
+//       vitalSpacing.WithSiteMargin,
+//       vitalSpacing.WithSiteXLConstraint
+//     ),
+//     OuterContainer: 'min-h-screen',
+//   }
+// });
 
-const VitalLayout = as(
-  VanillaLayout,
-)(LayoutClean);
+// const VitalLayout = as(
+//   VanillaLayout,
+// )(LayoutClean);
+
+// const Layout = ({ children }) => (
+//   <VitalLayout>
+//     <main>{children}</main>
+//   </VitalLayout>
+// );
 
 const Layout = ({ children }) => (
-  <VitalLayout>
+  <div>
     <main>{children}</main>
-  </VitalLayout>
+  </div>
 );
 
 export default Layout;
