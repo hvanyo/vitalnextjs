36689ef47fab2fd92553a4fb24acc403bb0701d4
hvanyo <hvanyo@its.jnj.com>
2022-10-21 15:44:44 -0500

    Add test

--- src/pages/styleguide/card/index.jsx
+++ src/pages/styleguide/card/index.jsx
@@ -106,7 +106,7 @@ const SEO = as()(Div);
 const CardPage = () => (
 <Layout>
   <SEO title="Card" />
-  <H1title>Card Page</H1title>
+  <H1title>Card Page TEST</H1title>
   <p>Replication of Bodiless:Vital Design Card Page using Next.js</p>
   <Div className="md:w-1/2 mb-8">
     <Subtitle>Default Card</Subtitle>
